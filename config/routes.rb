c9d13c4115722dd1fd9ff6aabc88dd9640c6a2cd
vanheaven <vanheaven6@gmail.com>
2020-09-10 23:44:24 +0300

    Add style and markup to index view, registration and session new views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: %i[new create index]
-  devise_for :members
+  devise_for :members, :controllers => { registrations: 'registrations' }
   root 'posts#index'
   
 end
